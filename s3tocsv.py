6faeff5a3074550f0c110a1dd9eb06c0d03a50a6
Chris Holmes <chlmes@amazon.com>
2018-11-02 13:22:39 -0600

    working PoC

--- s3tocsv.py
+++ s3tocsv.py
@@ -1,63 +1,71 @@
+from __future__ import print_function
 import boto3
 import json
 import csv
-# from bson import json_util
 from datetime import datetime
-client = boto3.client('cloudwatch')
 
-# Query CloudWatch for a list of AWS/S3 metrics
-metrics_list = client.list_metrics(
-    Namespace='AWS/S3',
-    MetricName='BucketSizeBytes',
-)
+# Metric ID's can only contain alphanumeric characters.  drop all - and .'s
 def clean_metric_id(metric_id):
     clean = metric_id.replace("-","")
     clean = clean.replace(".","")
     return clean
-    
-# need to deserialize datetime
-def myconverter(o):
+
+# Deserialize datetime objects for debugging
+def dtconverter(o):
     if isinstance(o, datetime):
         return o.__str__()
-
-metric_queries = []
-for metric in metrics_list["Metrics"]:
-    metric_id = [x['Value'] for x in metric["Dimensions"] if x["Name"] == "BucketName"][0]
     
-    print(metric_id)
-    metric_queries.append({
-                'Id': clean_metric_id(metric_id),
-                'MetricStat': {
-                    'Metric': metric,
-                    'Period': 86400,
-                    'Stat': 'Average',
-                    'Unit': 'Bytes'
-                },
-            })
+# Query cloudwatch for metrics 
+def query_cloudwatch(start_time, end_time, metrics_list, client):
+    metric_queries = []
+    for metric in metrics_list["Metrics"]:
+        metric_id = [x['Value'] for x in metric["Dimensions"] if x["Name"] == "BucketName"][0]
+        
+        metric_queries.append({
+                    'Id': clean_metric_id(metric_id),
+                    'MetricStat': {
+                        'Metric': metric,
+                        'Period': 86400,
+                        'Stat': 'Average',
+                        'Unit': 'Bytes'
+                    },
+                })
+        
+    response = client.get_metric_data(
+            ScanBy='TimestampAscending',
+            MetricDataQueries=metric_queries,
+            StartTime=start_time,
+            EndTime=end_time)
+    # print(json.dumps(response['MetricDataResults'], indent=2, default=dtconverter))
+    return response
+
+def write_csv(metrics):
+    with open('buckets.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(
+            csvfile, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
+        
+        temprow = [' '] + [ x.date() for x in metrics['MetricDataResults'][0]["Timestamps"]]
+        csvwriter.writerow(temprow)
+
+        for metric in metrics['MetricDataResults']:
+            csvwriter.writerow([metric['Label']]+ metric['Values'])
+
+
+def main():
+    client = boto3.client('cloudwatch')
     
-response = client.get_metric_data(
-        MetricDataQueries=metric_queries,
-        StartTime=datetime(2018, 10, 1),
-        EndTime=datetime(2018, 10, 31))
+    # get list of metrics from CloudWatch
+    metrics_list = client.list_metrics(
+        Namespace='AWS/S3',
+        MetricName='BucketSizeBytes',
+    )
 
-print(response)
+    # specify start and end time
+    start_time = datetime(2018, 10, 1)
+    end_time = datetime(2018, 10, 31)
+    metrics = query_cloudwatch(start_time, end_time, metrics_list, client)
+    write_csv(metrics)
 
+if __name__ == '__main__':
+    main()
 
-
-    #ScanBy='TimestampDescending')
-
-    # with open('eggs.csv', 'w', newline='') as csvfile:
-    #     spamwriter = csv.writer(
-    #         csvfile, delimiter=',', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
-    #     row = ['']
-    #     for result in response['MetricDataResults']:
-    #         for stamp in result["Timestamps"]:
-    #             row.append(stamp)
-
-    #         spamwriter.writerow(row)
-    #         row = ['']
-
-    #         for val in result['Values']:
-    #             row.append(val)
-
-    #         spamwriter.writerow(row)
